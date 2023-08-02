d48e46e364840541c6eb0a7d523cc882e8e8f0b0
eduardo-kosinski <edu_f97@outlook.com>
2023-08-02 13:54:57 -0300

    Function to return a random word from database

--- hangman.py
+++ hangman.py
@@ -1,10 +1,29 @@
+import random
+
 def play():
     print_entrance()
+
+    secret_word = random_word()
+    # print(secret_word)
 
 def print_entrance():
     print("***********************");
     print("* Welcome to Hangman! *");
     print("***********************");
+
+def random_word():
+    file = open("wordbank.txt", "r")
+    words = []
+
+    for line in file:
+        line = line.strip()
+        words.append(line)
+
+    file.close()
+
+    number = random.randrange(0, len(words))
+    secret_word = words[number].upper()
+    return secret_word
 
 if(__name__ == "__main__"):
     play()
