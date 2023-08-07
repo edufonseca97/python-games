8ecf1cbb3918a4ca2455ed1c6b689b89f2703ecb
eduardo-kosinski <edu_f97@outlook.com>
2023-08-07 13:26:06 -0300

    Core of hangman active

--- hangman.py
+++ hangman.py
@@ -9,6 +9,28 @@ def play():
     right_letters = secret_letters(secret_word)
     print(right_letters)
 
+    finish = False
+    errors = 0
+
+    while(not finish and errors < 7):
+
+        guess = player_guess()
+
+        if(guess in secret_word):
+            # change right letters
+            print(guess)
+        else:
+            errors += 1
+
+        finish = "_" not in right_letters
+
+        print(right_letters)
+
+    if(finish):
+        print("Congratulations!")
+    else:
+        print("You lost!")
+
 def print_entrance():
     print("***********************");
     print("* Welcome to Hangman! *");
@@ -30,6 +52,11 @@ def random_word():
 
 def secret_letters(word):
     return ["_" for letter in word]
+
+def player_guess():
+    guess = input("Guess a letter: ")
+    guess = guess.strip().upper()
+    return guess
 
 if(__name__ == "__main__"):
     play()
